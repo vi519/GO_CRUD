9a106081db16943e1dda4b522de420f0b78f4604
Vineet Mishra <vineet@Vineets-MacBook-Air.local>
2025-04-26 18:12:18 +0530

    ORM-Addition

--- config/db.go
+++ config/db.go
@@ -13,30 +13,44 @@ import (
 var DB *sql.DB
 
 func Connect() {
+	// Load environment variables from .env file
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
 
+	// Get the DB connection details from environment variables
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("DB_NAME")
 
+	// Create the connection string
 	connStr := fmt.Sprintf(
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		dbHost, dbPort, dbUser, dbPassword, dbName,
 	)
 
+	// Open the DB connection
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // Log the error and terminate the application
 	}
 
+	// Check if the connection is established successfully
 	if err = db.Ping(); err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // Log the error and terminate the application
 	}
 
+	// Set the global DB variable
 	DB = db
+
+	// Ensure DB connection is closed gracefully when the app exits
+	// So that connections are properly cleaned up when app terminates
+	defer func() {
+		if err := DB.Close(); err != nil {
+			log.Println("Error closing DB connection:", err)
+		}
+	}()
 }
---
+++ models/account.go
@@ -0,0 +1,8 @@
+package models
+
+type Account struct {
+	ID            int    `json:"id"`
+	AccountNumber int    `json:"accountnumber"`
+	Name          string `json:"name"`
+	Email         string `json:"email"`
+}
